681f1ccaa1f3be33a787aa60fa006da7dd48b852
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-01-04 09:08:53 +0530

    section-1-functionality-achieved

---
+++ restaurant-the-food-hub/script.js
@@ -0,0 +1,17 @@
+const iconsList = document.querySelectorAll(".section-1-icons i");
+let itr = 1;
+
+setInterval(() => {
+  itr++;
+
+  const icon = document.querySelector(".section-1-icons .change");
+
+  icon.classList.remove("change");
+
+  if (itr > iconsList.length) {
+    iconsList[0].classList.add("change");
+    itr = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
+  }
+}, 1000);
