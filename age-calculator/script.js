7882a3efa6fca36cb58ad86de983ef2d5182dc48
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-11-10 22:33:32 +0530

    calculate-difference-in-day

--- age-calculator/script.js
+++ age-calculator/script.js
@@ -1,7 +1,8 @@
 let userInput = document.getElementById("date");
-userInput.max = new Date().toISOString.split("T")[0];
+// userInput.max = new Date().toISOString.split("T")[0];
+let result = document.getElementById("result");
 
-document.querySelector("button").addEventListener("click", () => {
+document.querySelector(".btn").addEventListener("click", () => {
   let birthDate = new Date(userInput.value);
 
   let birthDay = birthDate.getDate();
@@ -17,10 +18,29 @@ document.querySelector("button").addEventListener("click", () => {
   let differenceDay, differenceMonth;
   let differenceYear = currentYear - birthYear;
 
+  // Calculate month
   if (currentMonth >= birthMonth) {
     differenceMonth = currentMonth - birthMonth;
   } else {
     differenceYear--;
     differenceMonth = 12 + currentMonth - birthMonth;
   }
+
+  // Calculate day
+  if (currentDay >= birthDay) {
+    differenceDay = currentDay - birthDay;
+  } else {
+    differenceDay--;
+    differenceDay =
+      getDaysInMonth(birthYear, birthMonth) + currentDay - birthDay;
+  }
+
+  if (differenceMonth < 0) {
+    differenceMonth = 11;
+    differenceYear--;
+  }
 });
+
+function getDaysInMonth(year, month) {
+  return new Date(year, month, 0).getDate();
+}
