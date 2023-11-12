f20662f04f7d549ea7920a8575c22ebaa48261b4
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-11-12 17:51:08 +0530

    insert-list-items-into-DOM

--- drag-and-drop/script.js
+++ drag-and-drop/script.js
@@ -13,3 +13,31 @@ const richestPeople = [
   "Kumar Birla",
   "Shapoor Mistry",
 ];
+
+// store listItems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// ! Insert list items into DOM
+function createList() {
+  [...richestPeople].forEach((person, index) => {
+    const listItem = document.createElement("li");
+
+    listItem.setAttribute("data-index", index);
+
+    listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+        <p class="person-name">${person}</p>
+        <i class="fas fa-grip-lines"></i>
+    </div>
+    `;
+
+    listItems.push(listItem);
+
+    draggableList.append(listItem);
+  });
+}
