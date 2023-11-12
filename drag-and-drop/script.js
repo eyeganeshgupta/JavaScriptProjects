daae6766e980b6a0ace31667c2dda46d9491b085
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-11-12 17:43:34 +0530

    top-10-richest-people-of-india

---
+++ drag-and-drop/script.js
@@ -0,0 +1,15 @@
+const draggableList = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "Mukesh Ambani",
+  "Gautam Adani",
+  "Shiv Nadar",
+  "Savitri Jindal",
+  "Radhakishan Damani",
+  "Cyrus Poonawalla",
+  "Hinduja Family",
+  "Dilip Shanghvi",
+  "Kumar Birla",
+  "Shapoor Mistry",
+];
