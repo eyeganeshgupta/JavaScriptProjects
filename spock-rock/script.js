e0334e0427848f4ee816a259b0c1c73b259d4650
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-11-30 01:06:11 +0530

    add-selected

--- spock-rock/script.js
+++ spock-rock/script.js
@@ -52,7 +52,6 @@ function resetAll() {
   resultText.textContent = "";
   resetSelected();
 }
-
 window.resetAll = resetAll;
 
 // TODO: Random computer choice
@@ -68,5 +67,33 @@ function computerRandomChoice() {
     computerChoice = "lizard";
   } else {
     computerChoice = "spock";
+  }
+}
+
+// TODO: Add 'selected' styling & computerChoice
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case "rock":
+      computerRock.classList.add("selected");
+      computerChoiceEl.textContent = " --- Rock";
+      break;
+    case "paper":
+      computerPaper.classList.add("selected");
+      computerChoiceEl.textContent = " --- Paper";
+      break;
+    case "scissors":
+      computerScissors.classList.add("selected");
+      computerChoiceEl.textContent = " --- Scissors";
+      break;
+    case "lizard":
+      computerLizard.classList.add("selected");
+      computerChoiceEl.textContent = " --- Lizard";
+      break;
+    case "spock":
+      computerSpock.classList.add("selected");
+      computerChoiceEl.textContent = " --- Spock";
+      break;
+    default:
+      break;
   }
 }
