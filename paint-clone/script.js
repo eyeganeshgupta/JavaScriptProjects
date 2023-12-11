a947b552ef553d5b4196278a2e2cc7f102523c84
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-12-11 22:15:01 +0530

    restore-canvas

--- paint-clone/script.js
+++ paint-clone/script.js
@@ -91,3 +91,20 @@ clearCanvasBtn.addEventListener("click", () => {
   activeToolEl.textContent = "Canvas Cleared";
   setTimeout(switchToBrush, 1500);
 });
+
+// Draw what is stored in DrawnArray
+function restoreCanvas() {
+  for (let i = 1; i < drawnArray.length; i++) {
+    context.beginPath();
+    context.moveTo(drawnArray[i - 1].x, drawnArray[i - 1].y);
+    context.lineWidth = drawnArray[i].size;
+    context.lineCap = "round";
+    if (drawnArray[i].eraser) {
+      context.strokeStyle = bucketColor;
+    } else {
+      context.strokeStyle = drawnArray[i].color;
+    }
+    context.lineTo(drawnArray[i].x, drawnArray[i].y);
+    context.stroke();
+  }
+}
