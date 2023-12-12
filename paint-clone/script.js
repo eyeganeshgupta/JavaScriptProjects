d39645b659d0d1ea3fde5e29da0fd98c60fcf9bd
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-12-12 19:05:18 +0530

    mouse-down-eventListener

--- paint-clone/script.js
+++ paint-clone/script.js
@@ -129,3 +129,14 @@ function getMousePosition(event) {
     y: event.clientY - boundaries.top,
   };
 }
+
+// Mouse Down
+canvas.addEventListener("mousedown", (event) => {
+  isMouseDown = true;
+  const currentPosition = getMousePosition(event);
+  context.moveTo(currentPosition.x, currentPosition.y);
+  context.beginPath();
+  context.lineWidth = currentSize;
+  context.lineCap = "round";
+  context.strokeStyle = currentColor;
+});
