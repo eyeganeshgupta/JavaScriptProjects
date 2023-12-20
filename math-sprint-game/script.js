b68b404c4580864ed38353ef11f11a05f3b8bdc4
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-12-20 23:31:58 +0530

    refresh-splash-page

--- math-sprint-game/script.js
+++ math-sprint-game/script.js
@@ -40,3 +40,11 @@ let finalTimeDisplay = "0.0";
 
 // Scroll
 let valueY = 0;
+
+// Refresh Splash Page Best Scores
+function bestScoresToDOM() {
+  bestScores.forEach((bestScore, index) => {
+    const bestScoreEl = bestScore;
+    bestScoreEl.textContent = `${bestScoreArray[index].bestScore}s`;
+  });
+}
