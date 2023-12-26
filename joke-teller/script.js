04232bec9875a89ce058b92533247ee409dc961f
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-12-26 22:57:15 +0530

    voice-rss-speech-function

---
+++ joke-teller/script.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// VoiceRSS Speech Function
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, "%20");
+  // VoiceRSS Speech Parameters
+  VoiceRSS.speech({
+    key: "YOUR_API_KEY_HERE",
+    src: jokeString,
+    hl: "en-us",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
