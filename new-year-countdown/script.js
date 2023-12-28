0c60f28f78337f08b7b0e6761e9117389f5b0a1d
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-12-28 21:03:35 +0530

    set-background-year

--- new-year-countdown/script.js
+++ new-year-countdown/script.js
@@ -9,3 +9,6 @@ const loading = document.getElementById("loading");
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Set background year
+year.innerText = currentYear + 1;
