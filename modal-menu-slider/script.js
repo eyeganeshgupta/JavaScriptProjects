fabfb70cff29fbb445e95cfb3e98bea74afa4891
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-12-29 18:23:58 +0530

    show-and-hide-modal

---
+++ modal-menu-slider/script.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+// TODO: Toggle nav
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
+
+// TODO: Show modal
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+// TODO: Hide modal
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
